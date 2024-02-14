7a6573fe95ea8a455fddc23fc986a593218cae01
kobe <youcode@skiff.com>
2024-02-14 21:18:35 +0100

    remove vite

--- resources/views/user/projects/index.blade.php
+++ resources/views/user/projects/index.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Projects Overview</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script src="https://cdn.tailwindcss.com"></script>
+
 </head>
 <body class="bg-gray-100">
 <div class="container mx-auto mt-10">
